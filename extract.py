3f2df7a45b5b1d4ddceaa6c4db3502be862d8d64
sakrak-hub <sakra_k@outlook.com>
2025-10-03 19:11:35 +0800

    Complete revamp of extraction of data using SQL query

--- extract.py
+++ extract.py
@@ -2,26 +2,35 @@ import pandas as pd
 import requests
 import yaml
 import os
+from dotenv import load_dotenv
 
 yaml_file = 'mvc.yaml'
 
 with open(yaml_file, 'r') as f:
     mvc_data = yaml.load(f, Loader=yaml.FullLoader)
 
-def get_crashes_data(
-        base_url, 
-        offset=0, #by default the API limit is 1000 per request, so we use offset to 'page' through the data
-        date='2012-07-01' #hardcoding it for now, need to figure out how to dynamically get the earliest data point
-        ):
+def get_data(base_url, row_size=1000, datapage=1):
+
     url = base_url
-    params = {
-    "$limit" : 1000,
-    "$offset" : offset,
-    "crash_date" : date
+
+    headers = {
+        'X-App-Token': os.environ.get("X-APP-TOKEN")
     }
-    crashes_df = pd.json_normalize(requests.get(url, params = params).json())
-    file_path =f'./raw_data/crashes/{date}_crashes.csv' ##saving by date to seperate folders, 
-                                #we will have to choose between saving to csv first or direct upload to postgres
+
+    payload = {
+        'query': 'SELECT * ORDER BY crash_date ASC',
+        'page': {
+            'pageNumber': datapage,
+            'pageSize': row_size
+        }
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    if response.status_code == 200:
+        crashes_df = pd.json_normalize(requests.json())
+
+    file_path =f'./raw_data/crashes/pg_{datapage}_crashes.csv'
 
     if not os.path.exists(file_path):
         crashes_df.to_csv(file_path, index=False, mode='a', header=True)
