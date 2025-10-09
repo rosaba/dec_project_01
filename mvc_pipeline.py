735d6d85e6bff4a51c753aa4def956365953f376
sakrak-hub <sakra_k@outlook.com>
2025-10-09 15:32:25 +0800

    Backing up data after testing.

--- mvc_pipeline.py
+++ mvc_pipeline.py
@@ -24,9 +24,10 @@ if check_connection(source_url):
         latest_api_date = get_date(mvc_data[key],'SELECT * ORDER BY crash_date DESC LIMIT 1')
 
         if f'{key}' not in inspector.get_table_names():
-            latest_db_date = latest_api_date - timedelta(days=90) # <- first time upload, need to refactor to data from previous 90 days dynamically
+            latest_db_date = latest_api_date - timedelta(years=2) # <- first time upload, need to refactor to data from previous 90 days dynamically
         else:
             date_query = f'SELECT MAX(crash_date) from {key}'
+            logger.info("Updating data for the past 30 days")
             with engine.connect() as connection:
                 latest_db_date = connection.execute(text(date_query)).scalar() # <- subsequent uploads
                 metadata = MetaData()
@@ -39,6 +40,7 @@ if check_connection(source_url):
                     current_date-=timedelta(days=1)
 
         looping_date = latest_api_date # < - new data upload
+        logger.info("Uploading new data to database")
         while looping_date>latest_db_date:
             df = get_data(mvc_data[key], key, looping_date)
             save_to_csv(df,key,looping_date)
